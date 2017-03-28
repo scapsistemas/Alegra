f38d27109abb4a34e2fc5a252c78883f398437ea
scapsistemas <scap_sistemas@hotmail.com>
2017-03-27 23:41:24 -0400

    Update IndexController.php

--- IndexController.php
+++ IndexController.php
@@ -35,29 +35,10 @@ class IndexController extends AbstractActionController
         //$cmd='curl -v -H "Accept: application/json" -H "Content-type: application/json" -X DELETE  https://app.alegra.com/api/v1/contacts/' . $id .  ' -u "luiscarlgonzalez@hotmail.com:5c60262cb4d98e251346"';
        $cmd='curl -v -H "Accept: application/json" -H "Content-type: application/json" -X GET  https://app.alegra.com/api/v1/contacts/ -u "luiscarlgonzalez@hotmail.com:5c60262cb4d98e251346"'; 
        exec($cmd,$result);
-        
-        
-        return new ViewModel(
-               array(  'Elimina' => $result                 
-                   )                                
-                );
+
     }       
   
 public function redirAction(){
-    /*
-     * Refrescar la pagina
-     * return $this->redirect()->refresh();
-     */
-     
-    /*
-     * Redirigir a una ruta que tengamos definida en module.config.php
-     * return $this->redirect()->toRoute("home");
-     */
- 
-    /*
-     * Redirigir a una url
-     * obtenemos la url base y le indicamos donde ir
-     */
          return $this->redirect()->toUrl(
               $this->getRequest()->getBaseUrl().'/application/procesa/index'
          );
